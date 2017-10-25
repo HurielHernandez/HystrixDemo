f64da1fa2e64b0a20480fd7290e879ea1cfaad48
jackson.demaree <jackson.demaree@tamu.edu>
2017-10-25 11:26:40 -0400

    setup eureka and zuul

--- src/main/java/com/revature/HystrixDemoApplication.java
+++ src/main/java/com/revature/HystrixDemoApplication.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@EnableHystrix
 @RestController
+@EnableDiscoveryClient
 public class HystrixDemoApplication {
 
 	public static void main(String[] args) {
